34eb8952f09e6b478d8cc2107ecea4021c304c1f
gbhoot <gsbhoot1020@gnail.com>
2019-04-06 07:45:27 -0700

    server changes -database +need to create routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
 const express = require('express'),
+    path = require('path');
     bodyParser = require('body-parser'),
-    logger = require('morgan');
+    logger = require('morgan'),
+    API_PORT = 3001;
 var cors = require('cors');
 
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'static'));
+
+require('./server/config/routes.js')(app);
+
+var server = app.listen(API_PORT, () => {
+    console.log('LISTENING ON PORT ${API_PORT}');
+});
--- backend/server/config/mongoose.js
+++ backend/server/config/mongoose.js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose');
 
+const dbRoute = "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest";
+
+mongoose.connect(
+    dbRoute,
+    {useNewUrlParser: true}
+);
+
+let db = mongoose.connection;
+
+db.once('open', () => {
+    console.log("Connected to the database");
+});
+
+// Check if connection with the database is successful
+db.on('error', () => {
+    console.error.bind(console, "MonngoDB connection error");
+});
 
 module.exports = mongoose;
